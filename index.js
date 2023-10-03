0802688cfa769c612a6d5de5c7ce6f1c2454ab29
Ranjeet2006 <ranjeetjha205@gmail.com>
2023-10-03 22:28:39 +0530

    dom project

---
+++ index.js
@@ -0,0 +1,11 @@
+let div = document.getElementById("div1")
+div.style.height ="200px"
+
+div.style.width ="300px"
+div.style.backgroundColor ="yellow"
+div.style.margin ="20px"
+div.style.padding ="10px"
+
+div.style.fontSize ="18px"
+div.style.fontWeight ="bold"
+div.style.color="blue"
